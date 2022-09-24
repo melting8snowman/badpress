ee4cda53191b7752afeca0e7fa0b2a1784e60fcc
Harri Nieminen <nieminen.harri@gmail.com>
2022-09-24 16:43:48 +0300

    Initial create

---
+++ app.py
@@ -0,0 +1,20 @@
+# badpress app by HN
+from flask import Flask
+from flask import redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+#app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+## pages
+# initial
+@app.route("/")
+def index():
+    result = db.session.execute("SELECT content FROM messages")
+    messages = result.fetchall()
+    return render_template("index.html", count=len(messages), messages=messages) 
